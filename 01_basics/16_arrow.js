6c1feb4dffefba11a4309b3b13ce56a2db6af464
Rohan Sutradhar <125042488+Rohan029@users.noreply.github.com>
2026-06-04 19:43:14 +0530

    Add files via upload

---
+++ 01_basics/16_arrow.js
@@ -0,0 +1,70 @@
+const user = {
+    userName : "Rohan",
+    price: 999,
+    welcomeMessage : function () {
+    console.log(`${this.userName}, Welcome to the website, the price is ${this.price}`);
+        // console.log(this);
+        
+        
+    }
+}
+
+// user.welcomeMessage()
+
+// user.userName = "SAM"
+
+// // user.welcomeMessage()
+
+// // console.log(this);
+// function chai()
+// {
+//     let username = "Rohan"
+//     console.log(this.username);
+    
+// }
+
+// chai()
+
+
+// const chai = function(){
+//     let username = "Rohan"
+//     console.log(this.username);
+    
+// }
+
+// chai()
+
+
+// const chai = ()=> {
+//     let username = "Rohan"
+//     console.log(this.username);
+    
+// }
+
+// chai()
+
+// const addTwo = (num1, num2) => {
+//     return num1 + num2
+// }
+
+
+// const addTwo = (num1, num2) => (num1 + num2)
+// const addTwo = (num1, num2) => ({username:"Rohan"})
+
+// console.log(addTwo(3,5));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
