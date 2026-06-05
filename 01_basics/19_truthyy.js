3918ecf4fbb32da3c1961d06ad07f7ed55cbf11e
Rohan Sutradhar <125042488+Rohan029@users.noreply.github.com>
2026-06-05 16:20:14 +0530

    Add files via upload

---
+++ 01_basics/19_truthyy.js
@@ -0,0 +1,73 @@
+// const  userEmail = "Rohan@google.com"
+
+// if (userEmail){
+//     console.log("Got user Email");
+    
+// }
+// else {
+//     console.log("Dont have user email");
+    
+// }
+// const  userEmail = ""
+
+// if (userEmail){
+//     console.log("Got user Email");
+    
+// }
+// else {
+//     console.log("Dont have user email");
+    
+// }
+// const  userEmail = []
+
+// if (userEmail){
+//     console.log("Got user Email");
+    
+// }
+// else {
+//     console.log("Dont have user email");
+    
+// }
+
+//=============== falsy value ==============
+
+// false , 0 -0, BigInt 0n, "", null, undefined, NaN
+
+//============= truthy value =============
+
+// "0", 'false', " ", [], {}, funtion(){}
+
+//  const  userEmail = []
+
+// if (userEmail.length === 0){
+//     console.log("Arry is empty");
+    
+// }
+
+// const emptyObject = {}
+
+// if (Object.keys(emptyObject).length === 0){
+//     console.log("Object is empty");
+    
+// }
+
+
+
+// nullish Coalescing operator (??): null undefined
+
+// let var1;
+// // var1 = null ?? 55
+// var1 = undefined ?? 10
+// console.log(var1);
+
+
+// ========== terniary Operator ============
+
+// condition ? true: false 
+const iceTeaPrice = 100
+
+iceTeaPrice <= 80 ? console.log(`less than 80`): console.log(`more than 80`);
+
+
+
+
