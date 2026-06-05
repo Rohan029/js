fab997b567776ffd21ffc10d5cfd22f8dd1c310b
Rohan Sutradhar <125042488+Rohan029@users.noreply.github.com>
2026-06-05 16:20:38 +0530

    Add files via upload

---
+++ 01_basics/18_switch.js
@@ -0,0 +1,47 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+// const month = "3"
+
+// switch (month) {
+//     case 1:
+//         console.log("January");
+//         break;
+//     case 2:
+//         console.log("Februarhy");
+//         break;
+//     case 3:
+//         console.log("March");
+//         break;
+
+//     default:
+//         console.log("Does not match");
+        
+//         break;
+// }
+
+
+const month = "march"
+
+switch (month) {
+    case "Jan":
+        console.log("January");
+        break;
+    case "Feb":
+        console.log("Februarhy");
+        break;
+    case "march":
+        console.log("March");
+        break;
+
+    default:
+        console.log("Does not match");
+        
+        break;
+}
